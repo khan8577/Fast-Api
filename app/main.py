f706ab9a2c701e64f533c37c2bcb28314cbdbf89
khan8577 <khanayaz8577@gmail.com>
2025-08-13 11:44:21 +0500

    Removed old files and updated with new ones

--- app/main.py
+++ app/main.py
@@ -7,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 def hello(db: Session = Depends(get_db)):
     return {"message": "Hello from FastAPI to Supabase PostgreSQL!"}
+
+user = []
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+
+@app.post("/users/")
